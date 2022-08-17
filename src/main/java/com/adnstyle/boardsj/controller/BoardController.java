47d1ff4120e4565b2d2cb0b4e4b0cc0fffba44f1
tjdwns <tjdwns132@naver.com>
2022-08-17 16:46:32 +0900

    1차 작업 완료

--- src/main/java/com/adnstyle/boardsj/controller/BoardController.java
+++ src/main/java/com/adnstyle/boardsj/controller/BoardController.java
@@ -23,30 +23,40 @@ public class BoardController {
     private final BoardService boardService;
 
     /*
-    * 게시판 글 조회 처리
-    * */
+     * 게시판 글 조회 처리
+     * */
     @GetMapping("list.do")
-    public String listBoard(Model model){
+    public String listBoard(Model model) {
         List<BoardDto> boardList = boardService.listBoard();
-        model.addAttribute("boardList",boardList);
+        model.addAttribute("boardList", boardList);
         return "board/list";
     }
 
     /*
-    * 게시글 등록 처리
-    * */
+     * 게시글 등록 처리
+     * */
     @PostMapping("insertBoard.do")
-    public String insertBoard(BoardDto boardDto, HttpSession session){
+    public String insertBoard(BoardDto boardDto, HttpSession session) {
+        MemberDto memberInsertSession = (MemberDto) session.getAttribute("memberLoginInfo");
+        SessionUser userSession = (SessionUser) session.getAttribute("user");
+
+        if (memberInsertSession != null) {
+            String writer = memberInsertSession.getId();
+            String name = memberInsertSession.getName();
+            boardDto.setId(writer);
+            boardDto.setName(name);
+        } else {
+            String socialWriter = userSession.getEmail();
+            String socialName = userSession.getName();
+            boardDto.setId(socialWriter);
+            boardDto.setName(socialName);
+        }
+
         int result = 0;
-        MemberDto member=(MemberDto) session.getAttribute("memberLoginInfo");
-        String writer = member.getId();
-        String name = member.getName();
-        boardDto.setId(writer);
-        boardDto.setName(name);
         result += boardService.insertBoard(boardDto);
-        if(result>0){
+        if (result > 0) {
             return "redirect:/board/list.do";
-        }else {
+        } else {
             return "redirect:/board/post.do";
         }
     }
@@ -68,30 +78,30 @@ public class BoardController {
      * 답글 등록
      * */
     @PostMapping("insertBoardReplay.do")
-    public String insertBoardReplay(BoardDto boardDto, HttpSession session){
+    public String insertBoardReplay(BoardDto boardDto, HttpSession session) {
 //      일반회원 및 소셜회원 세션정보를 가져옴
-        MemberDto memberId = (MemberDto) session.getAttribute("memberLoginInfo");
-        SessionUser user = (SessionUser) session.getAttribute("user");
+        MemberDto memberSession = (MemberDto) session.getAttribute("memberLoginInfo");
+        SessionUser userSession = (SessionUser) session.getAttribute("user");
 
 //      일반회원 세션값이 존재하지 않을 경우 소셜회원 세션을 이용하여 게시판에 값을 입력해준다.
-        if(memberId!=null){
-            String replayWriter = memberId.getId();
-            String replayName = memberId.getName();
+        if (memberSession != null) {
+            String replayWriter = memberSession.getId();
+            String replayName = memberSession.getName();
             boardDto.setId(replayWriter);
             boardDto.setName(replayName);
-        }else{
-            String userId = user.getEmail();
-            String userName = user.getName();
-            boardDto.setId(userId);
-            boardDto.setName(userName);
+        } else {
+            String replaySocialWriter = userSession.getEmail();
+            String replaySocialName = userSession.getName();
+            boardDto.setId(replaySocialWriter);
+            boardDto.setName(replaySocialName);
         }
 //      답글 등록 전 게시물 순서를 위해 업데이트 이후 등록
-        int insertResult =0;
+        int insertResult = 0;
         insertResult += boardService.updateReplayBoard(boardDto);
         insertResult += boardService.insertBoardReplay(boardDto);
-        if(insertResult>0){
+        if (insertResult > 0) {
             return "redirect:/board/list.do";
-        }else{
+        } else {
             return "redirect:/board/replay/{seq}";
         }
     }
